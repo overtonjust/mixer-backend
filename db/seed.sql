7c61392b527ee89bdfd42302ec01dbfb9de756d8
eivor9 <156003182+eivor9@users.noreply.github.com>
2024-07-18 12:54:54 -0400

    Update seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO mixer(name, artist, album, genre, time_in_seconds, is_favorite, image, audio_url) VALUES
 ('It Boy', 'bbno$', 'It Boy - Single', 'Hip-Hop/Rap', 146, true, 'https://images.genius.com/2b5319db41b5d42d1038786b1652d3f5.1000x1000x1.png', 'https://www.youtube.com/watch?v=IflJ9d4kKJs'), -- bbno$
-('Mimi''s Delivery Service', 'Good Kid','Mimi''s Delivery Service - Single', 'Alternative', 179, false, 'https://i1.sndcdn.com/artworks-GFgTopPImdCZaOBy-Lek4aA-t500x500.jpg', 'https://www.youtube.com/watch?v=LMJSSYX1TFQ'),
+('Mimi''s Delivery Service', 'Good Kid','Mimi''s Delivery Service', 'Alternative', 179, false, 'https://i1.sndcdn.com/artworks-GFgTopPImdCZaOBy-Lek4aA-t500x500.jpg', 'https://www.youtube.com/watch?v=LMJSSYX1TFQ'),
 ('Betrayed By The Game', 'Dance Gavin Dance', 'Mothership', 'Rock/Metal', 191, true, 'https://m.media-amazon.com/images/I/81M4pGIEvEL._UF1000,1000_QL80_.jpg', 'https://www.youtube.com/watch?v=wBb4bwlD_bc'),
 ('Black Swan', 'BTS','Map of the Soul: 7', 'Pop/K-Pop', 199, false, 'https://m.media-amazon.com/images/I/61dMr8RTFvL._UF1000,1000_QL80_.jpg', 'https://www.youtube.com/watch?v=rKpmSZM0E54'),
 ('Never Gonna Give You Up', 'Rick Astley','Never Gonna Give You Up - Single', 'Pop/K-Pop', 212, false, 'https://m.media-amazon.com/images/I/81K8CCOEy3L._UF1000,1000_QL80_.jpg','https://www.youtube.com/watch?v=qvmP6PbC1JQ');
