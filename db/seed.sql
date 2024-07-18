94a432c5b1e98ab16385f576a3b9ff14968f0a8e
eivor9 <156003182+eivor9@users.noreply.github.com>
2024-07-18 11:34:19 -0400

    Update seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,8 +1,8 @@
 \c mixer_dev;
 
 INSERT INTO mixer(name, artist, album, genre, time_in_seconds, is_favorite, image, audio_url) VALUES
-('It Boy', 'bbno$', 'It Boy - Single', 'Pop/K-Pop', 146, true, 'https://t2.genius.com/unsafe/340x340/https%3A%2F%2Fimages.genius.com%2F2b5319db41b5d42d1038786b1652d3f5.1000x1000x1.png', 'https://www.youtube.com/watch?v=IflJ9d4kKJs'), -- bbno$
-('Mimi''s Delivery Service', 'Good Kid','Mimi''s Delivery Service - Single', 'Alternative', 179, false, 'https://t2.genius.com/unsafe/340x340/https%3A%2F%2Fimages.genius.com%2Fd68bb7c97ef7afceeee254396b81e39c.1000x1000x1.png', 'https://www.youtube.com/watch?v=LMJSSYX1TFQ'),
-('Betrayed By The Game', 'Dance Gavin Dance', 'Mothership', 'Rock/Metal', 191, true, 'https://t2.genius.com/unsafe/340x340/https%3A%2F%2Fimages.genius.com%2F5a8fe690bc77aeadfa5c1e518f0aa80a.1000x1000x1.jpg', 'https://www.youtube.com/watch?v=wBb4bwlD_bc'),
-('Black Swan', 'BTS','Map of the Soul: 7', 'Pop/K-Pop', 199, false, 'https://t2.genius.com/unsafe/340x340/https%3A%2F%2Fimages.genius.com%2F43dbb381553b8acd10de5695fc29d404.1000x1000x1.jpg', 'https://www.youtube.com/watch?v=mLcUA_d6Dwo'),
-('Never Gonna Give You Up', 'Rick Astley','Never Gonna Give You Up - Single', 'Pop/K-Pop', 212, false, 'https://t2.genius.com/unsafe/340x340/https%3A%2F%2Fimages.genius.com%2F88634fdafc60d4ff1e76944436c34a19.901x901x1.png','https://www.youtube.com/watch?v=qvmP6PbC1JQ');
+('It Boy', 'bbno$', 'It Boy - Single', 'Pop/K-Pop', 146, true, 'https://images.genius.com/2b5319db41b5d42d1038786b1652d3f5.1000x1000x1.png', 'https://www.youtube.com/watch?v=IflJ9d4kKJs'), -- bbno$
+('Mimi''s Delivery Service', 'Good Kid','Mimi''s Delivery Service - Single', 'Alternative', 179, false, 'https://i1.sndcdn.com/artworks-GFgTopPImdCZaOBy-Lek4aA-t500x500.jpg', 'https://www.youtube.com/watch?v=LMJSSYX1TFQ'),
+('Betrayed By The Game', 'Dance Gavin Dance', 'Mothership', 'Rock/Metal', 191, true, 'https://m.media-amazon.com/images/I/81M4pGIEvEL._UF1000,1000_QL80_.jpg', 'https://www.youtube.com/watch?v=wBb4bwlD_bc'),
+('Black Swan', 'BTS','Map of the Soul: 7', 'Pop/K-Pop', 199, false, 'https://m.media-amazon.com/images/I/61dMr8RTFvL._UF1000,1000_QL80_.jpg', 'https://www.youtube.com/watch?v=mLcUA_d6Dwo'),
+('Never Gonna Give You Up', 'Rick Astley','Never Gonna Give You Up - Single', 'Pop/K-Pop', 212, false, 'https://m.media-amazon.com/images/I/81K8CCOEy3L._UF1000,1000_QL80_.jpg','https://www.youtube.com/watch?v=qvmP6PbC1JQ');
