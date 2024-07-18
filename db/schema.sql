708b99f85f4b418fb2ce584e27dbb8fe5f25f4d4
eivor9 <156003182+eivor9@users.noreply.github.com>
2024-07-18 11:13:43 -0400

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -6,8 +6,8 @@ CREATE DATABASE mixer_dev;
 CREATE TABLE mixer (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
-    artist VARCHAR(30) NOT NULL,
-    album VARCHAR(30) NOT NULL,
+    artist VARCHAR(50) NOT NULL,
+    album VARCHAR(50) NOT NULL,
     genre TEXT NOT NULL,
     time_in_seconds INT CHECK (time_in_seconds > 0),
     is_favorite BOOLEAN,
